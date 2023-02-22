c824b5bf0cc0ddd4739edc69674706852bd19d3c
David C Kennedy <david.kennedy@mailfence.com>
2023-02-22 09:19:50 +0000

    Added a second led and an image

--- interference_led.py
+++ interference_led.py
@@ -2,10 +2,17 @@ from machine import Pin
 from machine import Timer
 
 pin1 = Pin(5, Pin.OUT)
+pin2 = Pin(4, Pin.OUT)
 
 pin1.on()
+pin2.on()
 
-tim1 = Timer(-1)
-tim2 = Timer(-1)
-tim1.init(period=5000, mode=Timer.PERIODIC, callback=lambda p: pin1.off())
-tim2.init(period=2000, mode=Timer.PERIODIC, callback=lambda p: pin1.on())
+led1tim1 = Timer(-1)
+led1tim2 = Timer(-1)
+led1tim1.init(period=5000, mode=Timer.PERIODIC, callback=lambda p: pin1.off())
+led1tim2.init(period=2000, mode=Timer.PERIODIC, callback=lambda p: pin1.on())
+
+led2tim1 = Timer(-1)
+led2tim2 = Timer(-1)
+led2tim1.init(period=450, mode=Timer.PERIODIC, callback=lambda p: pin2.off())
+led2tim2.init(period=187, mode=Timer.PERIODIC, callback=lambda p: pin2.on())
